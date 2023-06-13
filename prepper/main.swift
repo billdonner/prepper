5ecfb9e7be719b253c1641b3ae8e04ed97958438
Bill Donner <billdonner@gmail.com>
2023-06-13 12:24:20 -0500

    now produces script for validation of these blocks

--- prepper/main.swift
+++ prepper/main.swift
@@ -6,7 +6,17 @@ import Foundation
 import ArgumentParser
 import q20kshare
 
-
+struct TruthQuery :Codable {
+  let id:String
+  let question:String
+  let answer:String
+  let truth:Bool?
+}
+extension Challenge {
+  func makeTruthQuery ( ) -> TruthQuery {
+    TruthQuery(id: self.id ?? "farts", question:self.question, answer: self.correct, truth: nil)
+  }
+}
 var count : Int = 0
 var bytesRead : Int = 0
 var topicCounts: [String:Int] = [:]
@@ -125,12 +135,82 @@ fileprivate func cleanupChallenges(_ cha: [Challenge])->[Challenge] {
   return challenges
 }
 
-func writeJSONFile(_ urls:[String], outurl:URL)
+enum PrepperError: Error {
+  case badInputURL
+  case badOutputURL
+  case cantWrite
+  case noAPIKey
+}
+fileprivate func prep(_ x:String, initial:String) throws  -> FileHandle? {
+  if (FileManager.default.createFile(atPath: x, contents: nil, attributes: nil)) {
+    print(">Prepper created \(x)")
+  } else {
+    print("\(x) not created."); throw PrepperError.badOutputURL
+  }
+  guard let  newurl = URL(string:x)  else {
+    print("\(x) is a bad url"); throw PrepperError.badOutputURL
+  }
+  do {
+    let  fh = try FileHandle(forWritingTo: newurl)
+      fh.write(initial.data(using: .utf8)!)
+    return fh
+  } catch {
+    print("Cant write to \(newurl), \(error)"); throw PrepperError.cantWrite
+  }
+}
+
+fileprivate func writeAsPrompts<T:Encodable>(_ data: [T], _ outurl: URL) throws -> Int {
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted
+  var bc = 0
+  let fh = try prep(String(outurl.absoluteString.dropFirst(7)),initial: "*** Prepper run at \(Date())****")!
+  defer {
+    try? fh.close()
+  }
+  do {
+    for d in data {
+      let da  = try encoder.encode(d)
+      let json = String(data:da ,encoding: .utf8)
+      if let json  { 
+        let outs = "***\n\(json)\nAnswer with id ,true-or-false, and explanation as JSON\n\n"
+        bc += outs.count
+        fh.write(outs.data(using: .utf8)!)      }
+    }
+  }
+    catch {
+      print ("Can't write output \(error)")
+    }
+  return bc
+}
+fileprivate func writeAsJSON<T:Encodable>(_ data: [T], _ outurl: URL) throws -> Int {
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted
+  var bc = 0
+  do {
+    let data = try encoder.encode(data)
+    let json = String(data:data,encoding: .utf8)
+    if let json  {
+      bc = json.count
+      try json.write(to: outurl, atomically: false, encoding: .utf8)
+    }
+  }
+ 
+  return bc
+}
+
+func writeOutputFiles(_ urls:[String], gameFile:String)
 {
   var allChallenges:[Challenge] = []
   var topicCount = 0
   var fileCount = 0
   let start_time = Date()
+
+    let fj = gameFile + "-gamedata.json"
+    let fp = gameFile + "-prompts.txt"
+  let gamedataURL = URL(string:fj)
+  let promptsURL = URL(string:fp)
+  guard let promptsURL =  promptsURL, let gamedataURL = gamedataURL else { return }
+ 
   for url in urls {
     // read all the urls again
     guard let u = URL(string:url) else {
@@ -141,7 +221,7 @@ func writeJSONFile(_ urls:[String], outurl:URL)
       fileCount += 1
       let data = try Data(contentsOf: u)
       allChallenges =   fixupJSON(data:data,  url:u.absoluteString)
-       
+      
     }
     catch {
       print("Could not re-read \(u) error:\(error)")
@@ -176,29 +256,34 @@ func writeJSONFile(_ urls:[String], outurl:URL)
       gameDatum.append( GameData(subject:last,challenges: theseChallenges)) //include remainders
     }
     
-    // write Challenges as JSON to file
-    let encoder = JSONEncoder()
-    encoder.outputFormatting = .prettyPrinted
-    do {
-      let data = try encoder.encode(gameDatum)
-      let json = String(data:data,encoding: .utf8)
-      if let json  {
-        try json.write(to: outurl, atomically: false, encoding: .utf8)
-        let elapsed = Date().timeIntervalSince(start_time)
-        print(">Wrote \(json.count) bytes, \(allChallenges.count) challenges, \(topicCount) topics to \(outurl) in elapsed \(elapsed) secs")
+    // compute truth challenges
+    var cha:[TruthQuery] = []
+    for gd in gameDatum {
+      for ch in gd.challenges {
+        cha.append(ch.makeTruthQuery())
       }
     }
+    
+    do{
+      let ac = try writeAsPrompts(cha, promptsURL)
+      print(">Wrote \(ac) bytes \(cha.count) prompts to \(promptsURL)")
+      // write Challenges as JSON to file
+      let bc = try writeAsJSON(gameDatum, gamedataURL)
+      let elapsed = Date().timeIntervalSince(start_time)
+      print(">Wrote \(bc) bytes, \(allChallenges.count) challenges, \(topicCount) topics to \(gamedataURL) in elapsed \(elapsed) secs")
+    }
     catch {
-      print ("Can't write output \(error)")
+      print("Utterly failed to write files \(error)")
     }
   }
 }
+
 
 // MARK: - command line parsing with Argument Parser
 struct Prepper: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Read, Validate, and de-duplicate remote json Challenge files  specified as url arguments, supporting an -f filespec to generate a GamePlay ready json file",
-        version: "0.1.1",
+        version: "0.1.2",
         subcommands: [],
         defaultSubcommand: nil,
         helpNames: [.long, .short]
@@ -215,8 +300,8 @@ struct Prepper: ParsableCommand {
       print(">Prepper Command Line: \(CommandLine.arguments)")
       print(">Prepper running at \(Date())")
       analyze(urls)
-      if let filespec = gameFile , let furl = URL(string:filespec) {
-          writeJSONFile(urls, outurl:furl)
+      if let filespec = gameFile  {
+          writeOutputFiles(urls, gameFile:filespec)
         let elapsed = Date().timeIntervalSince(start_time)
         print(">Prepper finished in \(elapsed)secs")
         
